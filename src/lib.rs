db18a3faba95af745f906b6ee869bdfc82e52b4d
Michael Cuffaro <consulting@michaelcuffaro.com>
2021-06-26 11:22:11 -0400

    add json methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use std::fmt;
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +10,33 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+pub struct JSON {
+    content: String,
+}
+
+impl fmt::Display for JSON {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.content)?;
+        write!(f, "\n")?;
+
+        Ok(())
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, rdftab-wasm-poc!");
+impl JSON {
+    pub fn new(content: &str) -> JSON {
+        let content = content.to_string();
+        JSON {
+            content
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn thin_to_thick(&mut self) {
+        self.content = self.content.replace("plugh", "flugh").to_string();
+    }
 }
